627de3001a5a2872d9ded666f4c32e6a076189ee
chroswl <chroswl@gmail.com>
2026-07-16 01:07:41 +0200

    Microsoft Clarity 등록

--- src/analytics.ts
+++ src/analytics.ts
@@ -13,5 +13,15 @@ export const initAnalytics = () => {
       gtag('config', 'G-275E9CVL5E');
     `;
     document.head.appendChild(script2);
+    // Microsoft Clarity
+    const script3 = document.createElement('script');
+    script3.innerHTML = `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "xn25m6woyg");
+    `;
+    document.head.appendChild(script3);
   }
 };
